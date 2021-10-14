0ed9d3788449ae92285d8c2cc77806ff3f1346aa
Cesar <c.caycho46@gmail.com>
2021-10-13 19:21:40 -0500

    completed

--- main.go
+++ main.go
@@ -4,18 +4,22 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func main() {
-
-	httpsRouter := gin.Default()
+func setRedirectHTTPtoHTTPS() {
 	httpRouter := gin.Default()
-
 	httpRouter.GET("/*path", func(c *gin.Context) {
 		c.Redirect(302, "https://localhost:8081"+c.Request.RequestURI)
 	})
+	go httpRouter.Run(":8080")
+}
+
+func main() {
+
+	setRedirectHTTPtoHTTPS()
+
+	httpsRouter := gin.Default()
 
 	httpsRouter.GET("/", home)
-	go httpsRouter.RunTLS(":8081", "server.crt", "server.key")
-	httpRouter.Run(":8080")
+	httpsRouter.RunTLS(":8081", "server.crt", "server.key")
 }
 
 func home(c *gin.Context) {
